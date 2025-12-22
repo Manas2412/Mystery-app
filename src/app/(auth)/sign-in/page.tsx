e53ed55df578590ac0ef039d30cc60a050a2e782
Manas2412 <98726602+Manas2412@users.noreply.github.com>
2025-12-22 17:26:02 +0530

    React hook form, shadcn and debouncing Part-1

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,116 @@
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, useForm } from "react-hook-form"
+import { z } from "zod"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useDebounceValue } from 'usehooks-ts'
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import axios, { AxiosError } from 'axios'
+import { ApiResponse } from "@/types/ApiResponse"
+import { Message } from '../../../model/User';
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
+const page = () => {
+
+  const [username, setUsername] = useState("")
+  const [usernameMessage, setUsernameMessage] = useState("")
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const [debounceUsername] = useDebounceValue(username, 500)
+  const router = useRouter()
+
+  //zod implementation 
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: ''
+    }
+  })
+
+  useEffect(() => {
+    const checkUsernameUniquene = async () => {
+      if (debounceUsername) {
+        setIsCheckingUsername(true)
+        setUsernameMessage("")
+        try {
+          const response = await axios.get(`/api/check-username-unique?username=${debounceUsername}`)
+          setUsernameMessage(response.data.message)
+        } catch (err) {
+          const axiosError = err as AxiosError<ApiResponse>
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error checking username"
+          )
+        } finally {
+          setIsCheckingUsername(false)
+        }
+      }
+    }
+    checkUsernameUniquene()
+  }, [debounceUsername])
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true)
+    try {
+      const response = await axios.post<ApiResponse>('/api/sign-up', data)
+      toast.success("Success", {
+        description: response.data.message,
+      })
+      router.replace(`/verify/${data.username}`)
+      setIsSubmitting(false)
+    } catch (err) {
+      console.error("Error in signup of user", err)
+      const axiosError = err as AxiosError<ApiResponse>
+      let errorMessage = axiosError.response?.data.message
+      toast.error('Sign up failed', {
+        description: errorMessage ?? 'Something went wrong',
+      })
+      setIsSubmitting(false)
+    }
   }
+
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-orange-500 px-3 py-1 rounded-md m-4" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 spave-y-8 bg-white rounded-lg shadow-md">
+        <div className="text">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">
+            Sign up to Start your anonymous adventure
+          </p>
+        </div>
+
+        <Form {...form}>
+          <form onSubmit = {form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </form>
+        </Form>
+
+      </div>
+    </div>
   )
 }
+
+export default page
